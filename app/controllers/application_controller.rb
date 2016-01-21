e1a2b5a370232d7b79a058d741e937dff78a289e
Alan Davis <alan.davis@sage.com>
2016-01-21 04:19:30 +0000

    trying to do some iFrames

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  after_filter :allow_iframe
+  
+  def allow_iframe
+    response.headers["X-FRAME-OPTIONS"] = "ALLOW-FROM http://adavidev.com"
+  end
 end
